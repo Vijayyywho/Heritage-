c1185ab365e98dd469d0a82265b40432c22243bc
Vijayyy <112235237+Vijayyywho@users.noreply.github.com>
2025-08-17 16:51:33 +0530

    node

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'] // avoids Rollup/Linux build issues
-  }
+    exclude: ['lucide-react', '@rollup/rollup-linux-x64-gnu']
+  },
 });
